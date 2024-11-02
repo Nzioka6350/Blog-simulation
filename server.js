393b025fd77a84b8bb7f58b5b216bba067198d8a
Nzioka <115072481+Nzioka6350@users.noreply.github.com>
2024-11-02 18:59:48 +0300

    Start of the project

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 
 
-
+app.get('/',(req,res)=>{
+    res.send('Hello World!');
+})
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
