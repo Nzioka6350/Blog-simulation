c28b21bfe6955f26d39ffe220df0dce75d2a3d7e
Nzioka <115072481+Nzioka6350@users.noreply.github.com>
2024-11-06 23:28:32 +0300

    Database

--- Advanced/server.js
+++ Advanced/server.js
@@ -1,36 +1,11 @@
 import express from 'express';
-
 const app = express();
 const port = process.env.PORT  | 3000
 app.set('view engine', 'ejs');
 
 
-async function getNotes(){
-    const [rows] = await  pool.query("SELECT * FROM notes");
-    return rows
-}
-async function getNote(id){
-    const [rows] = await pool.query(
-        `
-        SELECT * FROM notes
-         WHERE id = ?
-        `,[id]
-    )
-    return rows[0];
-}
-const note = await getNote(4);
-console.log(note);
-async function createNote(title,content){
-    const [rows] = await pool.query(
-        `
-        INSERT INTO notes(title,content)
-        VALUES(?,?)
-        `,[title,content]
-    )
-    return rows;
-}
-const noted = await createNote("James","Test test");
-console.log(noted)
+
+
 
 app.listen(port,()=>{
     console.log("Application running on port",{port})
