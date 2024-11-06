ebe0d12cd1f40e6126853701261a43fe89f825f7
Nzioka <115072481+Nzioka6350@users.noreply.github.com>
2024-11-06 22:35:34 +0300

    Database

--- Advanced/server.js
+++ Advanced/server.js
@@ -1,17 +1,9 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import mysql from 'mysql2'
-dotenv.config();
+
 const app = express();
 const port = process.env.PORT  | 3000
 app.set('view engine', 'ejs');
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
 
-}).promise()
 
 async function getNotes(){
     const [rows] = await  pool.query("SELECT * FROM notes");
