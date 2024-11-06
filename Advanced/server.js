70a0c204f882136d9c6f11a3a84135907ca5cdc8
Nzioka <115072481+Nzioka6350@users.noreply.github.com>
2024-11-06 23:31:13 +0300

    Database

--- Advanced/server.js
+++ Advanced/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT  | 3000
 app.set('view engine', 'ejs');
-
+import {getNotes,getNote,createNote,deleteNote} from './database.js'
 
 
 
