64891387dc8e3b31f6ad860445529103cc602a33
Nzioka6350 <jamesnziokamutua1@gmail.com>
2024-12-04 13:27:06 +0300

    authentication

--- Full/database.js
+++ Full/database.js
@@ -1,8 +1,32 @@
 import mysql from 'mysql2';
-import dotdev from 'dotenv';
-dotdev.config;
+import dotenv from 'dotenv';
+dotenv.config();
 
 const db = mysql.createPool({
     host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
 }
 )
+
+export function getUser(id)
+{
+
+}
+export function getAllUsers()
+{
+
+}
+export function getNote(id)
+{
+
+}
+export function getNotes()
+{
+
+}
+export function createNote()
+{
+    
+}
